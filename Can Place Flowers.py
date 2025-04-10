ad89d70dd81de2e4d92d9cb187a7a454df09b6bf
Divya siddam <166330116+DivyaSiddam@users.noreply.github.com>
2025-04-10 19:29:00 -0400

    Can Place Flowers.py

--- Can Place Flowers.py
+++ Can Place Flowers.py
@@ -1,17 +1,14 @@
 #https://leetcode.com/problems/can-place-flowers/description/?envType=study-plan-v2&envId=leetcode-75
 class Solution:
-    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+    def canPlaceFlowers(self, flowerbed, n):
         count = 0
         for i in range(len(flowerbed)):
-            # Check if the current plot is empty.
             if flowerbed[i] == 0:
-                # Check if the left and right plots are empty.
                 empty_left_plot = (i == 0) or (flowerbed[i - 1] == 0)
-                empty_right_lot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
-                
-                # If both plots are empty, we can plant a flower here.
-                if empty_left_plot and empty_right_lot:
+                empty_right_plot = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
+                if empty_left_plot and empty_right_plot:
                     flowerbed[i] = 1
                     count += 1
-                    
+                    if count >= n:
+                        return True
         return count >= n
